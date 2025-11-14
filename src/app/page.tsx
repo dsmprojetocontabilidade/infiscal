ba7f9f167793c4e172d66673139ab8b6766dacbf
dsmprojetocontabilidade <dsmprojetocontabilidade@gmail.com>
2025-11-14 14:35:12 -0300

    Atualizações no tailwindcss da tabela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { Tabela } from "@/components/Tabela";
 
 export default function Home() {
   return (
-    <main className="flex-1 bg-zinc-900 flex-col p-4">
+    <main className="flex-1 bg-zinc-900 flex-col p-10 text-white">
       <div className="flex flex-col">
-        <h1 className="text-2xl mb-4 text-white">Acumuladores</h1>
+        <div className="flex flex-col p-0 mt-6 mb-6 gap-2">
+          <h1 className="text-2xl mb-4 text-white leading-0"> 📑Acumuladores cadastrados na Domínio</h1>
+          <h3 className="text-sm leading-0 ml-8">Guia de consulta rápida</h3>
+        </div>
         <Tabela />
       </div>
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export const Header = () => {
     return (
-        <header className="h-[70px] flex justify-between bg-zinc-900 p-4">
+        <header className="h-[70px] flex justify-between bg-zinc-900 py-4 px-10">
             <div className="">
                 <Image src="assets/logo.svg" width={100} height={50} alt="Logo InFiscal" />
             </div>
