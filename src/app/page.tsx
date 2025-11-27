6744dcea84596d23b832a6f4a693dbc509f60dee
dsmprojetocontabilidade <dsmprojetocontabilidade@gmail.com>
2025-11-27 11:33:12 -0300

    Criação de menu de link na home e adição dos primeiros dois links.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { Tabela } from "@/components/Tabela";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex-1 bg-zinc-900 flex-col p-10 text-white">
-      <div className="flex flex-col">
-        <div className="flex flex-col p-0 mt-6 mb-6 gap-2">
-          <h1 className="text-2xl mb-4 text-white leading-0"> 📑Acumuladores cadastrados na Domínio</h1>
-          <h3 className="text-sm leading-0 ml-8">Guia de consulta rápida</h3>
-        </div>
-        <Tabela />
+      <div>
+        <ol>
+          <li>1. <Link href="/acumuladores">Acumuladores</Link></li>
+          <li>2. EFD-REINF</li>
+        </ol>
       </div>
     </main>
   );
